d9469b8ad148a408de2cc4fed618a1fa3c6d0fc4
John <thebeefynugget@gmail.com>
2023-12-12 13:06:58 -0500

    fixed nav bar on mobile

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    const icon = document.querySelector('.navbar-toggler-icon');
+    icon.classList.toggle('open');
+});
